484cf11bce7596fc436b709d4cf2c25c9b1d13b5
Ryan Lane (Monsoonjr99) <ryanml1999@gmail.com>
2019-07-29 07:10:03 -0700

    a new command

--- monty.py
+++ monty.py
@@ -16,9 +16,13 @@ async def on_message(message):
 
     if message.content.lower().startswith('hello mabel'):
         await message.channel.send('Hello!')
-    elif message.content.lower().startswith(PREFIX+'hype'):
-        await message.channel.send('Hype is a subtropical depression!')
     elif message.content.lower().startswith(PREFIX):
-        await message.channel.send('test')
+        cmd = message.content[len(PREFIX):].split()
+        args = cmd[1:]
+        cmd = cmd[0].lower()
+        if cmd=='hype':
+            await message.channel.send('Hype is a subtropical depression!')
+        elif cmd=='monsoon':
+            await message.channel.send('A silly person')
 
 client.run(os.environ['BOT_TOKEN'])
