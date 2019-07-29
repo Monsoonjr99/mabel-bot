32440964b4b38c6c4e2942aa8a91b1d7cfbfeea9
Ryan Lane (Monsoonjr99) <ryanml1999@gmail.com>
2019-07-28 19:42:52 -0700

    starting

---
+++ monty.py
@@ -0,0 +1,22 @@
+import sys
+import discord
+TOKEN = sys.argv[1]
+PREFIX = 'm;'
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.lower().startswith('hello mabel'):
+        await message.channel.send('Hello!')
+    elif message.content.lower().startswith(PREFIX):
+        await message.channel.send('test')
+
+client.run(TOKEN)
