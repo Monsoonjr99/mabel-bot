ab0eac726e402020624b17dbbc8858db40ee171a
Ryan Lane (Monsoonjr99) <ryanml1999@gmail.com>
2019-07-30 10:59:21 -0700

    v20190730c

--- monty.py
+++ monty.py
@@ -1,8 +1,11 @@
 import sys
 import discord
 import os
+import random
 PREFIX = 'm;'
-VERSION = 'v20190730b'
+VERSION = 'v20190730c'
+
+random.seed()
 
 client = discord.Client()
 
@@ -22,10 +25,15 @@ async def on_message(message):
         args = cmd[1:]
         cmd = cmd[0].lower()
         if cmd=='version':
-            await message.channel.send('My version is %s'%VERSION)
+            await message.channel.send('My version is {}'.format(VERSION))
         elif cmd=='hype':
             await message.channel.send('Hype is a subtropical depression!')
         elif cmd=='monsoon':
-            await message.channel.send('A silly person')
+            mintiness = random.random()
+            if mintiness<0.0005:
+                mintiness = 0
+            else:
+                mintiness = random.randrange(1,100)
+            await message.channel.send('A very silly person who is currently {}% high on mints'.format(mintiness))
 
 client.run(os.environ['BOT_TOKEN'])
